f0da7ed9d8622c9eca759252faae7e3bd9eb40c7
ogeeDeveloper <omarogrant202@hotmail.com>
2022-02-02 13:12:58 -0500

    Added the Meetups on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
 
-const Home = () => {
-  return <div>Hello</div>;
+const DUMMY_DETAILS = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1549877452-9c387954fbc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    title: "Meetup1",
+    address: "123 lane, City1",
+    description: "This is the details for meetup one",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    title: "Meetup Two",
+    address: "154 lane, City2",
+    description: "This is the details for meetup two",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Meetup three",
+    address: "133 lane, City 3",
+    description: "This is the details for meetup three",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_DETAILS} />;
 };
 
-export default Home;
+export default HomePage;
--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const newMeetup = () => {
+const NewMeetupPage = () => {
   return <div></div>;
 };
 
-export default newMeetup;
+export default NewMeetupPage;
