830a992e356f53a0fe8e3eb663368df39ece7258
ogeeDeveloper <omarogrant202@hotmail.com>
2022-02-19 16:21:00 -0500

    Fix small bug in navigation

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,19 +1,20 @@
-import classes from './MainNavigation.module.css';
-import Link from "next/link"
+import classes from "./MainNavigation.module.css";
+import Link from "next/link";
 
 function MainNavigation() {
-
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link to="/">All Meetups</Link>
           </li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
+          <li>
+            <Link href="/new-meetup">Add New Meetup</Link>
           </li>
         </ul>
+      </nav>
     </header>
   );
 }
